137e72bbca6ea01fd1ea3f3f39e7984af231231a
Humza Hasan <humzahasan2101@gmail.com>
2020-03-29 20:04:13 +0530

    Layout change

--- src/Appeal.js
+++ src/Appeal.js
@@ -15,33 +15,63 @@ const Appeal = () => {
     let num = Math.ceil(Math.random()*precaution.length)
 
     return (
-        <div className="container-fluid">
-            <div className="card text-center">
-  <div className="row">
-      <div className="col">
-  <div className="card-body">
-    <h4 className="card-title text-primary">An Appeal</h4>        
-    <p className="card-text text-justify font-weight-bold">An Appeal to all the people of the world to stay safe, stay indoor and co-operate with the governing body in order to fight this global pandemic.</p>
-    </div>
-    </div>
-    <div className="col">
-    <div className="card-body">
-    <h4 className="card-title text-info">Thank You</h4>        
-    <p className="card-text text-justify font-weight-bold">A special word of thanks to all the people in the healthcare and emergency services who are doing their part to help us overcome this pandemic.</p>
-    </div>
-    </div>
-    <div className="col">
-    <div className="card-body">
-    <h4 className="card-title text-danger">Help Nation fight back</h4>        
-    <p className="card-text text-justify font-weight-bold">Do your part by donating for the <a href="https://www.pmindia.gov.in/en/?query#">PM CARES Fund</a>. We can all join hands and fight this together.</p>
-    </div>
-    </div>
-    
-  </div>
-</div>
-<p className="bg-success font-weight-bold ">{precaution[num]}</p>    
+        <div className="container-fluid mx-auto">
+        <div className="row mx-auto">
+            <div className="col mx-auto">
+            <div className="card border-0 mx-auto" >
+                <div className="card-body">
+                    <h4 className="card-title text-info">An Appeal</h4>        
+                    <p className="card-text text-justify font-weight-bold">An Appeal to all the people of the world to stay safe, stay indoor and co-operate with the governing body in order to fight this global pandemic.</p>    
+                </div>
+            </div>
+            </div>
+            <div className="col mx-auto">
+            <div className="card border-0 mx-auto" style={{width:"18rem"}}>
+                <div className="card-body">
+                    <h4 className="card-title text-success">Thank You</h4>        
+                    <p className="card-text text-justify font-weight-bold">A special word of thanks to all the people in the healthcare and emergency services who are doing their part to help us overcome this pandemic.</p>    
+                </div>
+            </div>
+            </div>
+            <div className="col mx-auto">
+            <div className="card border-0 mx-auto" >
+                <div className="card-body">
+                    <h4 className="card-title text-primary">Help Nation fight back</h4>        
+                    <p className="card-text text-justify font-weight-bold">Do your part by donating for the <a href="https://www.pmindia.gov.in/en/?query#">PM CARES Fund</a>. We can all join hands and fight this together.</p>    
+                </div>
+            </div>
+            </div>
+        </div>  
+        <br/>  
+        <p className="bg-success font-weight-bold ">{precaution[num]}</p>    
         </div>
     )
 }
 
 export default Appeal
+
+/*
+<div className="card text-center mx-auto">
+  <div className="row mx-auto">
+      <div className="col mx-auto">
+  <div className="card-body mx-auto">
+    <h4 className="card-title text-primary mx-auto">An Appeal</h4>        
+    <p className="card-text text-justify font-weight-bold mx-auto">An Appeal to all the people of the world to stay safe, stay indoor and co-operate with the governing body in order to fight this global pandemic.</p>
+    </div>
+    </div>
+    <div className="col mx-auto">
+    <div className="card-body mx-auto">
+    <h4 className="card-title text-info mx-auto">Thank You</h4>        
+    <p className="card-text text-justify font-weight-bold mx-auto">A special word of thanks to all the people in the healthcare and emergency services who are doing their part to help us overcome this pandemic.</p>
+    </div>
+    </div>
+    <div className="col mx-auto">
+    <div className="card-body mx-auto">
+    <h4 className="card-title text-danger mx-auto">Help Nation fight back</h4>        
+    <p className="card-text text-justify font-weight-bold mx-auto">Do your part by donating for the <a href="https://www.pmindia.gov.in/en/?query#">PM CARES Fund</a>. We can all join hands and fight this together.</p>
+    </div>
+    </div>
+    
+  </div>
+</div>
+*/
