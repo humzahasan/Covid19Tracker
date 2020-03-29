19fea36f6e2c9956c3d2efe7ab2f161a09ad3be0
Humza Hasan <humzahasan2101@gmail.com>
2020-03-29 12:47:53 +0530

    Navbar for mobile view fixed

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,6 +3,9 @@ import {Link} from 'react-router-dom'
 const Navbar = () => {
     return (
       <nav class="navbar navbar-expand-lg navbar-light bg-info">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
   <nav class="navbar-brand font-weight-bold">COVID-19 TRACKER</nav>
 
   <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
